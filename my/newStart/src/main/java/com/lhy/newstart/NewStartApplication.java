28db54ed56f6e66d5f574d1dd8600f6024699ad1
lihengyu <289989142@qq.com>
2025-07-22 10:11:12 +0800

    1

---
+++ my/newStart/src/main/java/com/lhy/newstart/NewStartApplication.java
@@ -0,0 +1,13 @@
+package com.lhy.newstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewStartApplication.class, args);
+    }
+
+}
